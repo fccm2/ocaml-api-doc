1dfd1cafac7b1bf4ae39db26d57f6b265b89cd84
Florent Monnier <monnier.florent@gmail.com>
2025-03-27 19:07:12 +0100

    comment module aliases (in Stdlib), that blocks producing the api-doc of this module

--- 4.11.1/stdlib.mli
+++ 4.11.1/stdlib.mli
@@ -1328,6 +1328,7 @@ val do_at_exit : unit -> unit
 (** {1:modules Standard library modules } *)
 
 (*MODULE_ALIASES*)
+(*
 module Arg          = Arg
 module Array        = Array
 module ArrayLabels  = ArrayLabels
@@ -1386,3 +1387,4 @@ module Sys          = Sys
 module Uchar        = Uchar
 module Unit         = Unit
 module Weak         = Weak
+*)
